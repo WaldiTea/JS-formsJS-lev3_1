e2421b4d4ac64a816849a89bcc4cc335ff19c137
SuperCoder <supercoder@MacBook-Air-von-SuperCoder.local>
2022-01-20 00:52:27 +0100

    third commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,6 @@
 function changeFontSize() {
   let output = document.getElementById('output');
-
   output.innerHTML = document.getElementById('input').value;
-
   output.style.fontSize = document.getElementById('range').value + 'px';
-
   output.style.fontFamily = document.getElementById('selector').value;
 }
